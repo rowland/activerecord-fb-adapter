69cc19b13ae5a05f2cc2950f475577260ecd180f
Brent Rowland <rowland@rowlandresearch.com>
2012-05-07 10:41:07 -0700

    Customize methods from DatabaseLimits module.

--- lib/active_record/connection_adapters/fb3_adapter.rb
+++ lib/active_record/connection_adapters/fb3_adapter.rb
@@ -749,6 +749,59 @@ module ActiveRecord
         end
       end
 
+    public
+      # from module DatabaseLimits
+
+      # the maximum length of a table alias
+      def table_alias_length
+        31
+      end
+
+      # the maximum length of a column name
+      def column_name_length
+        31
+      end
+
+      # the maximum length of a table name
+      def table_name_length
+        31
+      end
+
+      # the maximum length of an index name
+      def index_name_length
+        31
+      end
+
+      # the maximum number of columns per table
+      # def columns_per_table
+      #   1024
+      # end
+
+      # the maximum number of indexes per table
+      def indexes_per_table
+        65_535
+      end
+
+      # the maximum number of columns in a multicolumn index
+      # def columns_per_multicolumn_index
+      #   16
+      # end
+
+      # the maximum number of elements in an IN (x,y,z) clause
+      def in_clause_length
+        1499
+      end
+
+      # the maximum length of an SQL query
+      def sql_query_length
+        32767
+      end
+
+      # maximum number of joins in a single query
+      # def joins_per_query
+      #   256
+      # end
+
     end
   end
 end
