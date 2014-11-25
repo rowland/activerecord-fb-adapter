cb551851f1867d2d78d2c38a2bdb110e48391b03
Ray Zane <raymondzane@gmail.com>
2014-11-25 12:44:21 -0500

    Clean up ActiveRecord::Base.fb_connection

--- lib/active_record/fb_base.rb
+++ lib/active_record/fb_base.rb
@@ -1,20 +1,23 @@
 module ActiveRecord
   class Base
     def self.fb_connection(config) # :nodoc:
-      config = config.symbolize_keys.merge(:downcase_names => true)
-      unless config.has_key?(:database)
-        raise ArgumentError, "No database specified. Missing argument: database."
+      config = config.symbolize_keys.reverse_merge(:downcase_names => true)
+      fail ArgumentError, 'No database specified. Missing argument: database.' unless config[:database]
+      if db_host = config[:host]
+        config[:database] = File.expand_path(config[:database]) if db_host =~ /localhost/i
+        config[:database].prepend "#{db_host}/#{config[:port] || 3050}:"
       end
-      config[:database] = File.expand_path(config[:database]) if config[:host] =~ /localhost/i
-      config[:database] = "#{config[:host]}/#{config[:port] || 3050}:#{config[:database]}" if config[:host]
       require 'fb'
-      db = Fb::Database.new(config)
+      db = ::Fb::Database.new(config)
       begin
         connection = db.connect
       rescue
-        require 'pp'
-        pp config unless config[:create]
-        connection = config[:create] ? db.create.connect : (raise ConnectionNotEstablished, "No Firebird connections established.")
+        unless config[:create]
+          require 'pp'
+          pp config
+          fail ConnectionNotEstablished, "No Firebird connections established."
+        end
+        connection = db.create.connect
       end
       ConnectionAdapters::FbAdapter.new(connection, logger, config)
     end
